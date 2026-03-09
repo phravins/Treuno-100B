573797bd482d486e1b6506344267829a01da2495
phravins <164343676+phravins@users.noreply.github.com>
2026-03-09 09:22:43 +0530

    fix: replace deprecated tokenizer kwarg with processing_class in Trainer API

--- train.py
+++ train.py
@@ -108,7 +108,7 @@ def main():
         args=training_args,
         train_dataset=train_dataset,
         eval_dataset=eval_dataset,
-        tokenizer=tokenizer,
+        processing_class=tokenizer,
         data_collator=data_collator,
     )
     
